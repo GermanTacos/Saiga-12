1cfb0f38548c9a3719ed5e8846fdca5afb095eb3
IKillerwolfI <killerwolf.no1@gmail.com>
2018-09-26 01:19:17 +0200

    First ADS tweaks
    -yes

--- hooks/playertweakdata.lua
+++ hooks/playertweakdata.lua
@@ -1,9 +1,9 @@
-Hooks:PostHook( PlayerTweakData, "init", "VityazModStanceTweakData", function(self)
+Hooks:PostHook( PlayerTweakData, "init", "Saiga12GModStanceTweakData", function(self)
 
 --AK
 	
-	self.stances.heffy_919_ads = deep_clone(self.stances.flint)
+	self.stances.heffy_12g_ads = deep_clone(self.stances.flint)
 	local ironsight_translation = Vector3(0, -13, 0.518)
-	self.stances.heffy_919_ads.steelsight.shoulders.translation = self.stances.heffy_919_ads.steelsight.shoulders.translation + ironsight_translation 
+	self.stances.heffy_12g_ads.steelsight.shoulders.translation = self.stances.heffy_12g_ads.steelsight.shoulders.translation + ironsight_translation 
 
 end )

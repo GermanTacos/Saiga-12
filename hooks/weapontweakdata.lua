e76acdc1d1a6d6996570df00b97225a59dc42ea3
IKillerwolfI <killerwolf.no1@gmail.com>
2019-05-27 17:52:32 +0200

    Russian Sight CAP

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -113,7 +113,37 @@ if ( self.heffy_12g ) then
 	})
 	
 --- Magazines ---
-	
+	--- Russian Sights ---
+	self:SetupAttachmentPoint("heffy_12g", {
+		name = "a_o_1p29",
+		base_a_obj = "a_o",
+		position = Vector3(0.6, -13, 0.2),
+		rotation = Rotation(0, 0, 0)
+	})
+	self:SetupAttachmentPoint("heffy_12g", {
+		name = "a_o_1pn93",
+		base_a_obj = "a_o",
+		position = Vector3(-0.7, -13, 1.55),
+		rotation = Rotation(0, 0, 0)
+	})
+	self:SetupAttachmentPoint("heffy_12g", {
+		name = "a_o_ekp_1s_03",
+		base_a_obj = "a_o",
+		position = Vector3(0, -7, -1.2),	--	position = Vector3(-0.3, -2, -1.7),
+		rotation = Rotation(0, 0, 0)
+	})
+	self:SetupAttachmentPoint("heffy_12g", {
+		name = "a_o_okp7",
+		base_a_obj = "a_o",
+		position = Vector3(-0.5, -5.85, -2.2),
+		rotation = Rotation(0, 0, 0)
+	})
+	self:SetupAttachmentPoint("heffy_12g", {
+		name = "a_o_pso1",
+		base_a_obj = "a_o",
+		position = Vector3(-0.7, -7, -1.2),
+		rotation = Rotation(0, 0, 0)
+	})
 end
 end)
 
